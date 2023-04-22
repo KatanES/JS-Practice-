ccda45ef9d7fda20e8584c2bbcc5707f23b2af55
KatanES <businesskatan.es@gmail.com>
2023-04-22 10:07:20 +0200

    2nd quiz

--- JS/mainJS.js
+++ JS/mainJS.js
@@ -1,10 +1,21 @@
 // ЗАДАЧА 1
 // НАПИШІТЬ РЕЧЕННЯ ЗА ДОПОМОГОЮ ШАБЛОННОГО РЯДКА "ДОБРОГО ДНЯ,МЕНЕ ЗВАТИ ГАЛИНА, МЕНІ 35 РОКІВ, Я ВИВЧАЮ JAVASCRIPT"
 
-const name = "ГАЛИНА";
-const age = 35;
-const lang = "JAVASCRIPT";
+// const name = "ГАЛИНА";
+// const age = 35;
+// const lang = "JAVASCRIPT";
+
+// console.log(
+//   `ДОБРОГО ДНЯ, МЕНЕ ЗВАТИ ${name}, МЕНІ ${age} РОКІВ, Я ВИВЧАЮ ${lang}`
+// );
+
+// ЗАДАЧА 2
+// ЗАПИТАТИ У КОРИСТУВАЧА ІМЯ, ПОЧТУ , ТЕЛЕФОН, І ПОКАЗАТИ У КОНСОЛІ "КОРИСТУВАЧ __ ВИКОРИСТОВУЄ  __ ПОЧТУ І __ ТЕЛЕФОН"
+
+const name = prompt("Enter your name");
+const email = prompt("Enter your email");
+const phone = prompt("Enter your phone");
 
 console.log(
-  `ДОБРОГО ДНЯ, МЕНЕ ЗВАТИ ${name}, МЕНІ ${age} РОКІВ, Я ВИВЧАЮ ${lang}`
+  `КОРИСТУВАЧ ${name} ВИКОРИСТОВУЄ ${email} ПОЧТУ І ${phone} ТЕЛЕФОН`
 );
