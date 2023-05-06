bcd72d4b6283ed1ce00cba00d7fe88eb192af3f4
KatanES <businesskatan.es@gmail.com>
2023-05-06 11:56:41 +0200

    TASK 8

--- JS/JS2.js
+++ JS/JS2.js
@@ -112,3 +112,23 @@
 //   return `число ${b} більше ${a}`;
 // };
 // console.log(checkNumbers(10, 20));
+
+// ЗАДАЧА 1
+// Напишіть скрипт,для обєкта user в послідовності:
+//  добавляє поле mood із значенням 'happy'
+// замінює значення hobby на 'skydiving'
+// замінює значення  premium на false
+// виводить отримане обєкта user в форматі ключ:значення використовуючи  Object.keys() и for...of
+const user = {
+  name: "Mango",
+  age: 20,
+  hobby: "html",
+  premium: true,
+};
+user.mood = "happy";
+user.hobby = "skydiving";
+user.premium = false;
+const keys = Object.keys(user);
+for (let key of keys) {
+  console.log(key, user[key]);
+}
