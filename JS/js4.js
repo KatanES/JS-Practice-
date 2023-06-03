5db915683b1fc233901ca94d9ec88d8b2d9c2241
AntonTy35 <antony.tislenko@gmail.com>
2023-06-03 11:01:42 +0300

    6

---
+++ JS/js4.js
@@ -0,0 +1,23 @@
+// Завдання 6
+// Напиши скрипт, який під час втрати фокусу на інпуті (подія blur), перевіряє його вміст щодо правильної кількості введених символів.
+
+{
+  /* <input
+  type="text"
+  id="validation-input"
+  data-length="6"
+  placeholder="Please enter 6 symbols"
+/> */
+}
+
+// const inputEl = document.getElementById("validation-input");
+
+// inputEl.addEventListener("blur", () => {
+//   if (inputEl.value.length == inputEl.dataset.length) {
+//     inputEl.classList.add("valid");
+//     inputEl.classList.remove("invalid");
+//   } else {
+//     inputEl.classList.add("invalid");
+//     inputEl.classList.remove("valid");
+//   }
+// });
