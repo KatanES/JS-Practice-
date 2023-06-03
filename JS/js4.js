ab66689b67145ffa788e3f91c09dbf3809829327
KatanES <businesskatan.es@gmail.com>
2023-06-03 11:16:02 +0300

    task 4 (increment/decrement)

--- JS/js4.js
+++ JS/js4.js
@@ -21,3 +21,31 @@
 //     inputEl.classList.remove("valid");
 //   }
 // });
+
+// TASK 4
+// Лічильник складається зі спану і кнопок, які по кліку повинні збільшувати і зменшувати його значення на одиницю.
+
+// <div id="counter">
+//   <button type="button" data-action="decrement">-1</button>
+//   <span id="value">0</span>
+//   <button type="button" data-action="increment">+1</button>
+// </div>
+
+// Створи змінну counterValue, в якій буде зберігатися поточне значення лічильника та ініціалізуй її значенням 0.
+// Додай слухачів кліків до кнопок, всередині яких збільшуй або зменшуй значення лічильника.
+// Оновлюй інтерфейс новим значенням змінної counterValue.
+
+// let counterValue = 0;
+// const bttnDecrement = document.querySelector(`[data-action="decrement"]`);
+// const bttnIncrement = document.querySelector(`[data-action="increment"]`);
+// const value = document.querySelector("#value");
+
+// bttnDecrement.addEventListener("click", () => {
+//   counterValue -= 1;
+//   value.textContent = counterValue;
+// });
+
+// bttnIncrement.addEventListener("click", () => {
+//   counterValue += 1;
+//   value.textContent = counterValue;
+// });
