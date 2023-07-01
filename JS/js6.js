05ee3e4a429a37922176a7ef24cdf10ff81a0cb5
KatanES <businesskatan.es@gmail.com>
2023-07-01 11:52:03 +0200

    3d task

--- JS/js6.js
+++ JS/js6.js
@@ -26,39 +26,73 @@
 
 // const imgJs = document.querySelector(".img-js");
 
-// ЗАДАЧА 2
-// ВИКОРИСТОВУЮЧИ API ДАНОГО ПОСИЛАННЯ  https://www.thecocktaildb.com/api.php , ТА ЗА ДОПОМОГОЮ fetch, ВИВОДЬ РЕЗУЛЬТАТ НА ЕКРАН.
-// РЕЗУЛЬТАТ ПОШУКУ МОЖНА ОТРИМАТИ ІЗ ІМПУТА, КОЛИ КЛІЄНТ НАБРАВ ДАНИЙ ТОВАР(ВИКОРИСТАЙ РОЗМІТКУ ЗА ДОПОМОГОЮ ШАБЛОННОГО РЯДКА)
-
 // ЗАДАЧА 3
 // НАПИШІТЬ ПАГІНАЦІЮ, ДЛЯ ПЕРЕХОДУ ПО СТОРІНКАХ
 // https://docs.github.com/en/rest/search?apiVersion=2022-11-28#search-user
 //
 // fetch(`https://api.github.com/search/users?q=${}&client_id=67684cabc84f94f0938e&client_secret=782ea639550c1b5d986bdd8129813652ed04c92c`)
+// const form = document.getElementById("form");
+// const input = document.getElementById("input");
+// const item = document.querySelector(".item");
+// form.addEventListener("submit", foo);
+// let page = 1;
+// const loadBtn = document.querySelector(".load-js");
+// loadBtn.addEventListener("click", foo);
+
+// function foo(e) {
+//   e.preventDefault();
+//   const value = input.value;
+//   fetch(
+//     `https://api.github.com/search/users?q=${value}&client_id=67684cabc84f94f0938e&client_secret=782ea639550c1b5d986bdd8129813652ed04c92c&page=${page}`
+//   )
+//     .then((responce) => responce.json())
+//     .then((data) => mee(data.items))
+//     .then(() => page++)
+//     .catch((error) => console.log("Error!", error));
+// }
+
+// function boo({ html_url, avatar_url, login }) {
+//   const markUp = `<img src="${avatar_url}" alt="Тут должна быть Катя!">
+// <a href="${html_url}">LINK</a>
+// <h1>${login}</h1>`;
+//   item.insertAdjacentHTML("beforeend", markUp);
+// }
+// function mee(arr) {
+//   arr.forEach((e) => {
+//     return boo(e);
+//   });
+// }
+
+// html_url: "https://github.com/KatanES";
+// avatar_url: "https://avatars.githubusercontent.com/u/124148302?v=4";
+// login: "KatanES";
+
+// ЗАДАЧА 2
+// ВИКОРИСТОВУЮЧИ API ДАНОГО ПОСИЛАННЯ  https://www.thecocktaildb.com/api.php , ТА ЗА ДОПОМОГОЮ fetch, ВИВОДЬ РЕЗУЛЬТАТ НА ЕКРАН.
+// РЕЗУЛЬТАТ ПОШУКУ МОЖНА ОТРИМАТИ ІЗ ІМПУТА, КОЛИ КЛІЄНТ НАБРАВ ДАНИЙ ТОВАР(ВИКОРИСТАЙ РОЗМІТКУ ЗА ДОПОМОГОЮ ШАБЛОННОГО РЯДКА)
+
 const form = document.getElementById("form");
 const input = document.getElementById("input");
 const item = document.querySelector(".item");
 form.addEventListener("submit", foo);
-let page = 1;
-const loadBtn = document.querySelector(".load-js");
-loadBtn.addEventListener("click", foo);
 
 function foo(e) {
   e.preventDefault();
   const value = input.value;
-  fetch(
-    `https://api.github.com/search/users?q=${value}&client_id=67684cabc84f94f0938e&client_secret=782ea639550c1b5d986bdd8129813652ed04c92c&page=${page}`
-  )
+  fetch(`https://www.thecocktaildb.com/api/json/v1/1/search.php?s=${value}`)
     .then((responce) => responce.json())
-    .then((data) => mee(data.items))
-    .then(() => page++)
+    .then((data) => {
+      item.innerHTML = "";
+      mee(data.drinks);
+    })
     .catch((error) => console.log("Error!", error));
 }
 
-function boo({ html_url, avatar_url, login }) {
-  const markUp = `<img src="${avatar_url}" alt="Тут должна быть Катя!">
-<a href="${html_url}">LINK</a>
-<h1>${login}</h1>`;
+function boo({ strCategory, strDrinkThumb, strInstructions }) {
+  const markUp = `<img src="${strDrinkThumb}" alt="IMG!">
+<h2>${strCategory}</h2>
+  <p>${strInstructions}</p>;`;
+
   item.insertAdjacentHTML("beforeend", markUp);
 }
 function mee(arr) {
@@ -67,6 +101,8 @@ function mee(arr) {
   });
 }
 
-// html_url: "https://github.com/KatanES";
-// avatar_url: "https://avatars.githubusercontent.com/u/124148302?v=4";
-// login: "KatanES";
+// strCategory;
+// strDrinkThumb: "https://www.thecocktaildb.com/images/media/drink/71t8581504353095.jpg";
+// strInstructions;
+
+// https://www.loom.com/share/4b7a70df9aea467baba3c8a45b8fe2a6
